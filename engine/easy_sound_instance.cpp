a782921de8e7b0922416e0cc1dcef9b8549266e8
t800danya <t800@kvkozyrev.org>
2017-07-04 21:04:06 +0600

    Update easy_sound_instance.cpp

--- engine/easy_sound_instance.cpp
+++ engine/easy_sound_instance.cpp
@@ -20,6 +20,16 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 
+//t800 
+//
+// Copyright 2017  Bogdan Kozyrev t800@kvkozyrev.org
+// Desert Engine project
+
+#if defined (LINUX)
+#include <cstring>
+#endif
+//t800
+
 #include "easy_sound_instance.h"
 
 #include <memory>
